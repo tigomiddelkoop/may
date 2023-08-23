40f7ff8252b97ab2e7ed2b249b61bc28fcd94844
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2023-08-23 20:51:26 +0000

    Cleanup controllers, its better scoped now imo

---
+++ backend/app/Http/Controllers/VehicleController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Vehicle\StoreRequest as VehicleStoreRequest;
+use App\Http\Requests\Vehicle\UpdateRequest as VehicleUpdateRequest;
+use App\Models\Vehicle;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Vehicle::with(['engineType', 'vehicleType', 'defaultFuelType'])->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(VehicleStoreRequest $request)
+    {
+        $validated = $request->validated();
+        $vehicle = new Vehicle();
+
+        $vehicle->make = $validated['make'];
+        $vehicle->model = $validated['model'];
+
+        $vehicle->initial_kilometers = $validated['initial_kilometers'];
+        $vehicle->vin_number = $validated['vin_number'];
+
+        $vehicle->license_plate = $validated['license_plate'];
+        $vehicle->license_plate_country = $validated['license_plate_country'];
+
+        $vehicle->vehicleType()->associate($validated['vehicle_type']);
+        $vehicle->engineType()->associate($validated['engine_type']);
+        $vehicle->defaultFuelType()->associate($validated['default_fuel_type']);
+
+        // @TODO Switch to the user model for login
+        $vehicle->added_by = 1;
+
+        $vehicle->save();
+
+        return $vehicle->refresh();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Vehicle::with(['engineType', 'vehicleType', 'defaultFuelType'])->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(VehicleUpdateRequest $request, string $id)
+    {
+        $validated = $request->validated();
+        $vehicle = Vehicle::find($id)->update($validated);
+
+        // @TODO fix the assocs with relations when they change
+        // $vehicle->vehicleType()->associate($validated['vehicle_type']);
+        // $vehicle->engineType()->associate($validated['engine_type']);-
+        // $vehicle->defaultFuelType()->associate($validated['fuel_type']);
+
+        return Vehicle::find($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return Vehicle::destroy($id);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,34 +1,18 @@
 <?php
 
+// Auth
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Expense\DestroyController as ExpenseDestroyController;
-use App\Http\Controllers\Expense\IndexController as ExpenseIndexController;
-use App\Http\Controllers\Expense\ShowController as ExpenseShowController;
-use App\Http\Controllers\Expense\StoreController as ExpenseStoreController;
-use App\Http\Controllers\Expense\UpdateController as ExpenseUpdateController;
-use App\Http\Controllers\Fuel\DestroyController as FuelDestroyController;
-use App\Http\Controllers\Fuel\IndexController as FuelIndexController;
-use App\Http\Controllers\Fuel\ShowController as FuelShowController;
-use App\Http\Controllers\Fuel\StoreController as FuelStoreController;
-use App\Http\Controllers\Fuel\UpdateController as FuelUpdateController;
-use App\Http\Controllers\Location\DestroyController as LocationDestroyController;
-use App\Http\Controllers\Location\IndexController as LocationIndexController;
-use App\Http\Controllers\Location\ShowController as LocationShowController;
-use App\Http\Controllers\Location\StoreController as LocationStoreController;
-use App\Http\Controllers\Location\UpdateController as LocationUpdateController;
-use App\Http\Controllers\Route\DestroyController as RouteDestroyController;
-use App\Http\Controllers\Route\IndexController as RouteIndexController;
-use App\Http\Controllers\Route\ShowController as RouteShowController;
-use App\Http\Controllers\Route\StoreController as RouteStoreController;
-use App\Http\Controllers\Route\UpdateController as RouteUpdateController;
-use App\Http\Controllers\Vehicle\DestroyController as VehicleDestroyController;
-use App\Http\Controllers\Vehicle\IndexController as VehicleIndexController;
-use App\Http\Controllers\Vehicle\Overview\IndexController as VehicleOverviewIndexController;
-use App\Http\Controllers\Vehicle\Overview\ShowController as VehicleOverviewShowController;
-use App\Http\Controllers\Vehicle\ShowController as VehicleShowController;
-use App\Http\Controllers\Vehicle\StoreController as VehicleStoreController;
-use App\Http\Controllers\Vehicle\UpdateController as VehicleUpdateController;
+
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\FuelController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\RouteController;
+
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\Vehicle\OverviewController as VehicleOverviewController;
+use App\Http\Controllers\Vehicle\CategoryController as VehicleCategoryController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -55,58 +39,86 @@ Route::prefix('/auth')->name('auth.')->group(function () {
     Route::post('/forgot-password', ForgotPasswordController::class)->name('forgot-password');
 });
 
+Route::prefix('/users')->name('users.')->group(function () {
+
+});
+
 Route::middleware([])->group(function () {
     Route::prefix('/vehicles')->name('vehicles.')->group(function () {
+
         Route::prefix('/overview')->name('overview.')->group(function () {
-            Route::get('/', VehicleOverviewIndexController::class)->name('index');
-            Route::get('/{licence_plate}', VehicleOverviewShowController::class)->name('show');
+            Route::get('/', [VehicleOverviewController::class, 'index'])->name('index');
+            Route::get('/{licence_plate}', [VehicleOverviewController::class, 'show'])->name('show');
         });
 
-        Route::get('/', VehicleIndexController::class)->name('index');
+        Route::get('/', [VehicleController::class, 'index'])->name('index');
+        Route::get('/{id}', [VehicleController::class, 'show'])->name('show');
+        Route::post('/', [VehicleController::class, 'store'])->name('store');
+        Route::patch('/{id}', [VehicleController::class, 'update'])->name('update');
+        Route::delete('/{id}', [VehicleController::class, 'destroy'])->name('destroy');
 
-        Route::get('/{id}', VehicleShowController::class)->name('show');
-        Route::post('/', VehicleStoreController::class)->name('store');
-        Route::patch('/{id}', VehicleUpdateController::class)->name('update');
-        Route::delete('/{id}', VehicleDestroyController::class)->name('destroy');
+        // @TODO add categories
+
+        Route::prefix('/categories')->name('categories.')->group(function () {
+            Route::get('/', [VehicleCategoryController::class, 'index']);
+        });
+    });
+
+    Route::prefix('/expenses')->name('expenses.')->group(function () {
+        Route::get('/', [ExpenseController::class, 'index'])->name('index');
+
+        Route::get('/{id}', [ExpenseController::class, 'show'])->name('show');
+        Route::post('/', [ExpenseController::class, 'store'])->name('store');
+        Route::patch('/{id}', [ExpenseController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ExpenseController::class, 'destroy'])->name('destroy');
     });
 
     Route::prefix('/fuels')->name('fuels.')->group(function () {
-        Route::get('/', FuelIndexController::class)->name('index');
+        Route::get('/', [FuelController::class, 'index'])->name('index');
 
-        Route::get('/{id}', FuelShowController::class)->name('show');
-        Route::post('/', FuelStoreController::class)->name('store');
-        Route::patch('/{id}', FuelUpdateController::class)->name('update');
-        Route::delete('/{id}', FuelDestroyController::class)->name('destroy');
+        Route::get('/{id}', [FuelController::class, 'show'])->name('show');
+        Route::post('/', [FuelController::class, 'store'])->name('store');
+        Route::patch('/{id}', [FuelController::class, 'update'])->name('update');
+        Route::delete('/{id}', [FuelController::class, 'destroy'])->name('destroy');
+
+        // @TODO add categories
+        // @TODO add types
 
     });
 
     Route::prefix('/locations')->name('locations.')->group(function () {
-        Route::get('/', LocationIndexController::class)->name('index');
+        Route::get('/', [LocationController::class, 'index'])->name('index');
 
-        Route::get('/{id}', LocationShowController::class)->name('show');
-        Route::post('/', LocationStoreController::class)->name('store');
-        Route::patch('/{id}', LocationUpdateController::class)->name('update');
-        Route::delete('/{id}', LocationDestroyController::class)->name('destroy');
+        Route::get('/{id}', [LocationController::class, 'show'])->name('show');
+        Route::post('/', [LocationController::class, 'store'])->name('store');
+        Route::patch('/{id}', [LocationController::class, 'update'])->name('update');
+        Route::delete('/{id}', [LocationController::class, 'destroy'])->name('destroy');
+
+        // @TODO add categories
     });
 
-    Route::prefix('/expenses')->name('expenses.')->group(function () {
-        Route::get('/', ExpenseIndexController::class)->name('index');
+    Route::prefix('engines')->name('engines.')->group(function () {
+        Route::get('/', [EngineController::class, 'index'])->name('index');
 
-        Route::get('/{id}', ExpenseShowController::class)->name('show');
-        Route::post('/', ExpenseStoreController::class)->name('store');
-        Route::patch('/{id}', ExpenseUpdateController::class)->name('update');
-        Route::delete('/{id}', ExpenseDestroyController::class)->name('destroy');
+        Route::get('/{id}', [EngineController::class, 'show'])->name('show');
+        Route::post('/', [EngineController::class, 'store'])->name('store');
+        Route::patch('/{id}', [EngineController::class, 'update'])->name('update');
+        Route::delete('/{id}', [EngineController::class, 'destroy'])->name('destroy');
     });
+
 
     Route::prefix('/routes')->name('route.')->group(function () {
-        Route::get('/', RouteIndexController::class)->name('index');
+        Route::get('/', [RouteController::class, 'index'])->name('index');
 
-        Route::get('/{id}', RouteShowController::class)->name('show');
-        Route::post('/', RouteStoreController::class)->name('store');
-        Route::patch('/{id}', RouteUpdateController::class)->name('update');
-        Route::delete('/{id}', RouteDestroyController::class)->name('destroy');
+        Route::get('/{id}', [RouteController::class, 'show'])->name('show');
+        Route::post('/', [RouteController::class, 'store'])->name('store');
+        Route::patch('/{id}', [RouteController::class, 'update'])->name('update');
+        Route::delete('/{id}', [RouteController::class, 'destroy'])->name('destroy');
+
+        // @TODO add categories
     });
-    //
+
+
     //Route::prefix('/quick')->group(function () {
     //    Route::get()
     //})
