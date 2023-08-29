139013f74de3ff62288a8ab5bb9d798da5604099
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2023-08-29 19:51:25 +0000

    Revert "Move contents to the root as the frontend rep will be moved to another repo"
    This reverts commit 06a6cdffb3d4c580203833b57ae9eaf0d2790663.

--- backend/app/Models/Vehicle.php
+++ backend/app/Models/Vehicle.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vehicle extends Model
 {
@@ -24,10 +23,5 @@ class Vehicle extends Model
     public function vehicleType(): BelongsTo
     {
         return $this->belongsTo(VehicleType::class);
-    }
-
-    public function fuelExpenses(): HasMany
-    {
-        return $this->hasMany(FuelExpense::class);
     }
 }
